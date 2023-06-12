bae89cd250e80ff0ed3dc8a9bdda2043c623b067
arsenozhetov <a.ozhetov@arbuz.kz>
2023-06-12 19:20:53 +0600

    test: added tests

--- main.py
+++ main.py
@@ -30,12 +30,46 @@ def run_price_monitor(threshold: float) -> None:
 
 
 def get_current_price() -> float:
-    api_key: str = 'YOUR_BINANCE_API_KEY'
-    api_secret: str = 'YOUR_BINANCE_API_SECRET'
+    api_key: str = "YOUR_BINANCE_API_KEY"
+    api_secret: str = "YOUR_BINANCE_API_SECRET"
     client = Client(api_key, api_secret)
 
-    symbol: str = 'ETHUSDT'  # Symbol for ETH/USDT futures
+    symbol: str = "ETHUSDT"  # Symbol for ETH/USDT futures
     ticker = client.futures_mark_price(symbol=symbol)
-    current_price = float(ticker['markPrice'])
+    current_price = float(ticker["markPrice"])
 
     return current_price
+
+
+# Testing
+def test_check_price_change() -> None:
+    price_history1: list[float] = [100, 110, 105, 115, 120, 130]  # Change by 30%
+    assert check_price_change(price_history1, 10) is True
+    assert check_price_change(price_history1, 50) is True
+    assert check_price_change(price_history1, 100) is False
+
+    price_history2: list[float] = [100, 110, 105, 115, 120, 130, 135]  # Change by 35%
+    assert check_price_change(price_history2, 10) is True
+    assert check_price_change(price_history2, 40) is True
+    assert check_price_change(price_history2, 50) is False
+
+    price_history3: list[float] = [100, 110, 105, 115, 120, 130, 135, 132]  # Change by 32%
+    assert check_price_change(price_history3, 30) is True
+    assert check_price_change(price_history3, 35) is True
+    assert check_price_change(price_history3, 40) is False
+
+    price_history4: list[float] = [100, 110, 105, 115, 120, 130, 135, 132, 125]  # Change by 25%
+    assert check_price_change(price_history4, 20) is True
+    assert check_price_change(price_history4, 30) is False
+    assert check_price_change(price_history4, 50) is False
+
+
+def test_run_price_monitor() -> None:
+    threshold: float = 1  # Price change threshold - 1%
+    run_price_monitor(threshold)
+
+
+# Run the tests
+if __name__ == "__main__":
+    test_check_price_change()
+    test_run_price_monitor()
